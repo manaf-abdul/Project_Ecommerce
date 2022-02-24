322b461950eef5d4ab1e5ae61f0b2633e365ed5a
manaf-abdul <abdulmanafp1996@gmail.com>
2022-02-25 00:06:28 +0530

    changed database to cloud

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,13 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const connectDB=async()=>{
 
     try {
         // console.log('enter')
-        const conn = await mongoose.connect('mongodb://localhost:27017/multistoreDB') 
+        const conn = await mongoose.connect(process.env.MONGODB_URI) 
         console.log(`MongoDB connected:${conn.connection.host}`)
     } catch (error) {
         console.log(`Error:${error.message}`)
